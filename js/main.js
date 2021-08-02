d41e66f6443b55877da456e61f6cf2f1e72057a9
olyamarker <krasnova047@gmail.com>
2021-08-02 10:20:34 +0400

    create: modal windows&validation

--- js/main.js
+++ js/main.js
@@ -69,3 +69,23 @@ var swiper2 = new Swiper('.reviews__swiper-container', {
     prevEl: '.reviews__slider-button--prev',
   },
 });
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+	
+	function openModal() {
+		var modalOverlay = $('.modal__overlay');
+		var modalDialog = $('.modal__dialog');
+		modalOverlay.addClass('modal__overlay--visible');
+		modalDialog.addClass('modal__dialog--visible');
+		};
+
+	function closeModal(event) {
+		event.preventDefault();
+		var modalOverlay = $('.modal__overlay');
+		var modalDialog = $('.modal__dialog');
+		modalOverlay.removeClass('modal__overlay--visible');
+		modalDialog.removeClass('modal__dialog--visible');
+		};
